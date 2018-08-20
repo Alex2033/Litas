275e3a998c02d87cd385505bb6f111071b888c77
springmelody <aleksandrzakharov91@gmail.com>
2018-08-20 13:59:15 +0300

    second commit

---
+++ app/js/common.js
@@ -0,0 +1,245 @@
+$('.order-btn').click(function() {
+
+    $('.request').fadeIn(200);
+
+});
+
+$('.exit-btn, .request__background').click(function() {
+
+    $('.request').fadeOut(200);
+
+});
+
+$('.accordions__button').click(function () {
+
+    $(this).toggleClass('active');
+    $('.accordions__item + .accordions__button').not($(this).next()).slideUp(400);
+    $(this).next().slideToggle(400);
+
+});
+
+$(function() {
+
+    $('.hero__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: ".hero__arrow--left",
+        nextArrow: ".hero__arrow--right",
+        dotsClass: "slick-buttons",
+        customPaging : function(slider, i) {
+            var thumb = $(slider.$slides[i]).data('thumb');
+            return '<button class="slick-button"></button>';
+        },
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                prevArrow: false,
+                nextArrow: false,
+              }
+            },
+        ]
+    });
+
+});
+
+$(function() {
+
+    $('.clients__slider').slick({
+        infinite: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        dotsClass: "slick-buttons",
+        customPaging : function(slider, i) {
+            var thumb = $(slider.$slides[i]).data('thumb');
+            return '<button class="slick-button"></button>';
+        },
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+        ]
+    });
+
+});
+
+$(document).ready(function () {
+    updateContainer();
+});
+
+$(window).resize(function() {
+    setTimeout(function() {
+        updateContainer();
+    }, 1500);
+    
+});
+
+$('.header__toggle').click(function() {
+
+    $('.menu').slideToggle(400);
+
+});
+
+function updateContainer() {
+    var $containerWidth = $(window).width();
+        if ($containerWidth <= 767) {
+
+            var windowWidth = $(window).width();
+            $('.search__button').click(function(){
+                var windowWidth = $(window).width();
+                var block_width = $('.search__input').width();
+
+                $('.search__input').css({
+                    'width': (windowWidth / 1.2) - 60 + 'px',
+                    'opacity': '1'
+                })
+                
+                
+            });
+
+            $(document).mouseup(function (e) {
+                var windowWidth = $(window).width();
+                var block_width = $('.search__input').width();
+
+                var container = $('.search');
+                    if (!container.is(e.target) && container.has(e.target).length === 0) {
+                    $('.search__input').css({
+                        'width': '10px',
+                        'opacity': '0'
+                    });
+                }
+                
+            });
+
+            $('.navigation__list').click(function() {
+                $('.menu').fadeOut();
+            })
+    }
+    else {
+        $('.search__button').unbind('click');
+        $('.navigation__list').unbind('click');
+    }
+}
+
+/* INNER PAGES */
+
+$(document).ready(function () {
+    sidebarUpdate();
+});
+
+$(window).resize(function() {
+    setTimeout(function() {
+        sidebarUpdate();
+    }, 500);
+    
+});
+
+function sidebarUpdate() {
+
+
+
+    var $windowWidth = $(window).width();
+
+        if ($windowWidth <= 992) {
+
+            var toplogo_pos = $('.sidebar').offset().top; // определяем его первоначальное положение
+            $(window).scroll(function(){
+
+               if ($(window).scrollTop() > toplogo_pos){ // Если страницу прокрутили дальше, чем находится наш блок
+                    $('.sidebar').css('position', 'fixed'); // То мы этот блок фиксируем и отображаем сверху.
+            
+               } else{  // Если же позиция скрола меньше (выше), чем наш блок
+                    $('.sidebar').css('position', 'absolute'); // то возвращаем все назад
+               }
+            });
+
+        }
+
+};
+
+$('.inner-menu__toggle').click(function() {
+    $('.sidebar').toggleClass('active');
+    $('.hide').toggleClass('active');
+});
+
+$('.hide').click(function() {
+    $('.sidebar').toggleClass('active');
+    $('.hide').toggleClass('active');
+});
+
+$('.sidebar-menu__button').click(function() {
+    $('.hidden-production').slideToggle(300);
+
+});
+
+$('.services__sidebar-toggle').click(function() {
+    $('.services-hidden').slideToggle(300);
+});
+
+$('.services__sidebar-button').click(function() {
+    $('.hidden-button').toggleClass('hidden');
+
+    if ( $('.services-hidden').css('display') == 'block' ) {
+        $('.hidden-production').css('display', 'none');
+    }
+});
+
+$('.hidden-button').click(function() {
+    $('.services__sidebar-button').toggleClass('hidden');
+});
+
+$(function() {
+
+    // Инициализация карты
+    var myMap;
+    // при успешной загрузке API выполняется функция инициализации карты
+    ymaps.ready(init);
+
+    function init(){
+        // создание экземпляра карты и его привязка к контейнеру с id="map"
+        myMap = new ymaps.Map("map", {
+            // центр карты
+            center: [59.907271, 30.262553],
+            // коэффициент масштабирования
+            zoom: 14,
+            controls: []
+            // тип карты, по умолчанию используется тип карты "схема"
+            //type: "yandex#map",
+        });
+        myMap.behaviors.disable('scrollZoom');
+     
+        /*myMap.controls.add("zoomControl", {
+            position: {top: 15, left: 15}
+        });*/
+     
+        var myPlacemark = new ymaps.Placemark([59.907271, 30.262553] , {},
+            { iconLayout: 'default#image',
+              // iconImageHref: 'http://blog.karmanov.ws/files/APIYaMaps1/min_marker.png',
+              iconImageSize: [40, 51],
+              iconImageOffset: [-20, -47] });     
+     
+        myMap.geoObjects.add(myPlacemark);
+    }
+
+});
+
+$(function() {
+
+    if ($(window).width() <= 480) {
+        var size = 60,
+        newsContent= $('.news__text-right p'),
+        newsText = newsContent.text();
+        
+        if(newsText.length > size){
+            newsContent.text(newsText.slice(0, size) + ' ...');
+        }
+    } 
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+var syntax        = 'sass';
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browsersync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync'),
+		pug           = require('gulp-pug'),
+		htmlmin       = require('gulp-htmlmin'),
+		imagemin      = require('gulp-imagemin'),
+		gulpSequence  = require('gulp-sequence'),
+		wait          = require('gulp-wait');
+
+gulp.task('browser-sync', function() {
+	browsersync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// tunnel: true,
+		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('pug', function() {
+	return gulp.src('app/pug/pages/*.pug')
+	.pipe(pug({
+		pretty: true
+	}))
+	.pipe(gulp.dest('app'))
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/sass/main.sass')
+	.pipe(wait(1000))
+	.pipe(sass({ 'include css': true }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick/slick.min.js',
+		'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('transfer', function(){
+    return gulp.src([
+		"app/**",
+		"!app/pug",
+		"!app/sass",
+		"!app/js/common.js",
+	])
+        .pipe(gulp.dest("dist"))
+});
+
+gulp.task('html-minify', function() {
+	return gulp.src('app/*.html')
+	  .pipe(htmlmin({collapseWhitespace: true}))
+	  .pipe(gulp.dest('dist'));
+  });
+
+gulp.task('imgmin', () =>
+  gulp.src('app/img/**/*')
+	  .pipe(imagemin())
+	  .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task('build', gulpSequence('transfer', 'html-minify', 'imgmin'));
+
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Includes files to deploy
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', ['pug', 'styles', 'js', 'browser-sync'], function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/pug/*/*.pug', ['pug']);
+	gulp.watch('app/*.html', browsersync.reload);
+});
+
+gulp.task('default', ['watch']);
